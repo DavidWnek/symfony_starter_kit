c4d333f0a65f0269e94bdab13507302792681b6c
David Wnek <davidwnek@gmail>
2017-08-12 11:38:39 -0500

    Added more user Fixtures. Added Admin link to base template.

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -2,22 +2,43 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
+use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Entity\User;
 
-class LoadUserData implements FixtureInterface
+class LoadUserData extends AbstractFixture implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $userAdmin = new User();
-        $userAdmin->setUsername('admin');
-        $userAdmin->setPlainPassword('1234');
-        $userAdmin->setEmail('admin@localhost.com');
-        $userAdmin->setRoles(array('ROLE_SUPER_ADMIN'));
-        $userAdmin->setEnabled(true);
+        $users = array(
+          'user' => array(
+            'email' => '%s@localhost.com',
+            'role' => 'ROLE_USER',
+          ),
+          'admin' => array(
+            'email' => '%s@localhost.com',
+            'role' => 'ROLE_ADMIN',
+          ),
+          'superadmin' => array(
+            'email' => '%s@localhost.com',
+            'role' => 'ROLE_SUPER_ADMIN',
+          ),
+        );
 
-        $manager->persist($userAdmin);
-        $manager->flush();
+        foreach($users as $name => $u) {
+          $user = new User();
+          $user->setUsername($name);
+          $user->setEmail(sprintf($u['email'], $name));
+          $user->setPlainPassword('1234');
+          $user->setSalt(sha1($name.time()));
+          $user->setRoles(array($u['role']));
+          $user->setEnabled(true);
+
+          $this->setReference('user_'.$name, $user);
+
+          $manager->persist($user);
+          $manager->flush();
+        }
     }
 }
